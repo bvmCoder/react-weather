99a6fa6923afdd96cfbd1aea5f56246239593f38
Dixit Patel <talkwithdixit@gmail.com>
2016-12-23 12:44:47 -0500

    Added Foundation-sites css-loaders style-loaders

--- server.js
+++ server.js
@@ -19,3 +19,10 @@ app.use(express.static('public'));
 app.listen(PORT, function () {
   console.log(`Express server is up on port: ${PORT}`);
 });
+
+/*
+ npm i css-loader@0.23.1 script-loader@0.6.1 style-loader@0.13.0 jquery@2.2.1
+ foundation-sites@6.2.0 --save-dev
+
+ foundation-sites is breaking at version 6.3.0
+*/
