9893e7923ec944614bae511fbadc33087418863b
@bvmCoder <talkwithdixit@gmail.com>
2016-12-24 13:12:50 -0500

    Updated ClassNames files to take the new Changes

--- app/ClassNames/index.js
+++ app/ClassNames/index.js
@@ -1,10 +1,70 @@
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  var hasOwn = {}.hasOwnProperty;
+    var hasOwn = {}.hasOwnProperty;
 
-  var ClassNames = {
+    var mdcClassnames = {
 
-  };
+        setCssModule: function() {
+            var classes = [];
 
-})();
+            for (var i = 0; i < arguments.length; i++) {
+                var arg = arguments[i];
+                if (!arg) continue;
+
+                var argType = typeof arg;
+
+                if (argType === 'string' || argType === 'number') {
+                    classes.push(this && this[arg] || arg);
+                } else if (Array.isArray(arg)) {
+                    classes.push(setCssModule.apply(this, arg));
+                } else if (argType === 'object') {
+                    for (var key in arg) {
+                        if (hasOwn.call(arg, key) && arg[key]) {
+                            classes.push(this && this[key] || key);
+                        }
+                    }
+                }
+            }
+
+            return classes.join(' ');
+        },
+
+        classSet: function() {
+            var classes = [];
+
+            for (var i = 0; i < arguments.length; i++) {
+                var arg = arguments[i];
+                if (!arg) continue;
+
+                var argType = typeof arg;
+
+                if (argType === 'string' || argType === 'number') {
+                    classes.push(arg);
+                } else if (Array.isArray(arg)) {
+                    classes.push(classSet.apply(null, arg));
+                } else if (argType === 'object') {
+                    for (var key in arg) {
+                        if (hasOwn.call(arg, key) && arg[key]) {
+                            classes.push(key);
+                        }
+                    }
+                }
+            }
+
+            return classes.join(' ');
+        }
+    };
+
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = mdcClassnames;
+    } else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+        // register as 'mdcClassnames', consistent with npm package name
+        define('mdcClassnames', [], function () {
+            return mdcClassnames;
+        });
+    } else {
+        window.mdcClassnames = mdcClassnames;
+    }    
+
+}());
